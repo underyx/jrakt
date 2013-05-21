b0fccd2678047901cdf1a0bdbcae428909cd63c6
Nagy Bence <underer@gmail.com>
2013-05-22 00:40:30 +0200

    started parsing rating pages

--- jrakt.py
+++ jrakt.py
@@ -1,9 +1,24 @@
 from config import *
+import re
 import requests
 from bs4 import BeautifulSoup
 
 s = requests.session()
 s.get('https://www.jinni.com')
-s.post('https://www.jinni.com/jinniLogin', data={'user': username, 'pass': password, 'loginOverlayURL': '', 'loginSource': 'loginOverlay'})
-r = s.get('http://www.jinni.com/user/%s/ratings/' % username)
-print r.text
+s.post('https://www.jinni.com/jinniLogin', {'user': username, 'pass': password, 'loginOverlayURL': '', 'loginSource': 'loginOverlay'})
+
+movielinks = []
+def getRatingsPage(page=2):
+    r = s.post('https://www.jinni.com/user/%s/ratings/' % username, {'userRatingForm': 'userRatingForm', 'userRatingForm:j_id269': 'idx%s' % page, 'userRatingForm:j_id269idx%s' % page: 'userRatingForm:j_id269idx%s' % page})
+
+
+    ratings_page = BeautifulSoup(r.text)
+
+    for div in ratings_page(id=re.compile('ratingStrip\[\d+\]')):
+        movielinks.append('https://www.jinni.com%s' % div.a.get('href'))
+
+    if ratings_page.find(class_='next_rev'):
+        print ratings_page.find(class_='activePage').string
+
+getRatingsPage()
+print movielinks
