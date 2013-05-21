29eb0b2f279f851bbd983dd126898ad50151fa84
Nagy Bence <underer@gmail.com>
2013-05-21 23:35:05 +0200

    jinni authentication

--- jrakt.py
+++ jrakt.py
@@ -1,5 +1,9 @@
 from config import *
-import requests as rs
+import requests
 from bs4 import BeautifulSoup
 
-pass
+s = requests.session()
+s.get('https://www.jinni.com')
+s.post('https://www.jinni.com/jinniLogin', data={'user': username, 'pass': password, 'loginOverlayURL': '', 'loginSource': 'loginOverlay'})
+r = s.get('http://www.jinni.com/user/%s/ratings/' % username)
+print r.text
