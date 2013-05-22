9781d302528b6ffde160f9af7c71f342c97034d6
Nagy Bence <underer@gmail.com>
2013-05-22 17:39:23 +0200

    reworked total page counter

--- jrakt.py
+++ jrakt.py
@@ -1,31 +1,35 @@
 from config import *
+from math import ceil
 import re
 import requests
 from bs4 import BeautifulSoup
 
+print 'Initializing...'
 s = requests.session()
 s.get('http://www.jinni.com')
+print 'Logging in as %s' % username
 s.post('http://www.jinni.com/jinniLogin', {'user': username, 'pass': password, 'loginOverlayURL': '', 'loginSource': 'loginOverlay'})
+print 'Logged in as %s' % username
 
 movielinks = []
 
 
-def getRatingsPage(page=1, viewstate=None):
+def getRatingsPage(page=1, viewstate=None, total_pages=None):
     r = s.post('http://www.jinni.com/user/%s/ratings/' % username, data={'userRatingForm': 'userRatingForm', 'userRatingForm:j_id269': 'idx%s' % page, 'javax.faces.ViewState': viewstate})
 
     ratings_page = BeautifulSoup(r.text)
 
-    # Total pages are calculated like so:
-    # Get number from last word of .scrollerText (ex. 'Showing 1 - 50 of 361')
-    # Divide them by 50 (the number of results on one page)
-    # Convert result to integer, which will always round down
-    # Add one, sicne we wanted to round it up
-    total_pages = int(float(ratings_page.find(class_='scrollerText').string.split()[-1]) / 50) + 1
+    if not total_pages:
+        # Get last word of .scrollerText (ex. 'Showing 1 - 50 of 36'), divide it
+        # by 50 (number of results on one page) and get ceiling of the result
+        total_pages = int(ceil(float(ratings_page.find(class_='scrollerText').string.split()[-1]) / 50))
+
     print 'Fetching movie links from page %s of %s' % (page, total_pages)
 
     for div in ratings_page(id=re.compile('ratingStrip\[\d+\]')):
         movielinks.append('http://www.jinni.com%s' % div.a.get('href'))
-    if ratings_page.find(class_='next_rev'):
+
+    if page < total_pages:
         viewstate = ratings_page.find(id='javax.faces.ViewState').get('value')
         getRatingsPage(page + 1, viewstate)
 
