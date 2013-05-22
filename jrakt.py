a45e9fa148d68a60ade32530248aa0a6ce3d9bd9
Nagy Bence <underer@gmail.com>
2013-05-22 17:10:18 +0200

    added support for pagination

--- jrakt.py
+++ jrakt.py
@@ -4,21 +4,30 @@ import requests
 from bs4 import BeautifulSoup
 
 s = requests.session()
-s.get('https://www.jinni.com')
-s.post('https://www.jinni.com/jinniLogin', {'user': username, 'pass': password, 'loginOverlayURL': '', 'loginSource': 'loginOverlay'})
+s.get('http://www.jinni.com')
+s.post('http://www.jinni.com/jinniLogin', {'user': username, 'pass': password, 'loginOverlayURL': '', 'loginSource': 'loginOverlay'})
 
 movielinks = []
-def getRatingsPage(page=2):
-    r = s.post('https://www.jinni.com/user/%s/ratings/' % username, {'userRatingForm': 'userRatingForm', 'userRatingForm:j_id269': 'idx%s' % page, 'userRatingForm:j_id269idx%s' % page: 'userRatingForm:j_id269idx%s' % page})
 
+
+def getRatingsPage(page=1, viewstate=None):
+    r = s.post('http://www.jinni.com/user/%s/ratings/' % username, data={'userRatingForm': 'userRatingForm', 'userRatingForm:j_id269': 'idx%s' % page, 'javax.faces.ViewState': viewstate})
 
     ratings_page = BeautifulSoup(r.text)
 
-    for div in ratings_page(id=re.compile('ratingStrip\[\d+\]')):
-        movielinks.append('https://www.jinni.com%s' % div.a.get('href'))
+    # Total pages are calculated like so:
+    # Get number from last word of .scrollerText (ex. 'Showing 1 - 50 of 361')
+    # Divide them by 50 (the number of results on one page)
+    # Convert result to integer, which will always round down
+    # Add one, sicne we wanted to round it up
+    total_pages = int(float(ratings_page.find(class_='scrollerText').string.split()[-1]) / 50) + 1
+    print 'Fetching movie links from page %s of %s' % (page, total_pages)
 
+    for div in ratings_page(id=re.compile('ratingStrip\[\d+\]')):
+        movielinks.append('http://www.jinni.com%s' % div.a.get('href'))
     if ratings_page.find(class_='next_rev'):
-        print ratings_page.find(class_='activePage').string
+        viewstate = ratings_page.find(id='javax.faces.ViewState').get('value')
+        getRatingsPage(page + 1, viewstate)
 
 getRatingsPage()
 print movielinks
