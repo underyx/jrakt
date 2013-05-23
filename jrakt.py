8d15e16c722b79660fc7eb00b565f57eeffe96ab
Nagy Bence <underer@gmail.com>
2013-05-24 00:28:36 +0200

    reworked to use a Title class, added seen title submitting

--- jrakt.py
+++ jrakt.py
@@ -2,23 +2,48 @@ from config import *
 from math import ceil
 import re
 import requests
+import json
+import hashlib
 from bs4 import BeautifulSoup
+
+
+class Title:
+    def __init__(self, link, rating):
+        self.link = link
+        self.rating = rating
+        self.imdbid = None
+        self.titletype = None  # 'movie' or 'show'
+        self.timestamp = None
+
+        self.fixZeroRating()
+
+    def fixZeroRating(self):
+        if self.rating == 0:
+            self.rating == 1
+
+    def getIMDbID(self):
+        print 'Fetching IMDb ID of %s' % self.link
+        movie_page = BeautifulSoup(j.get(self.link).text)
+        imdbidindex = int(str(movie_page).find('http://www.imdb.com/title/')) + 26
+        self.imdbid = str(movie_page)[imdbidindex:imdbidindex+9]
+
+    def setType(self, titletype):
+        self.titletype = titletype
 
 
 def openJinniSession():
     print 'Initializing...'
-    jinni = requests.session()
-    jinni.get('http://www.jinni.com')
+    j = requests.session()
+    j.get('http://www.jinni.com')
     print 'Logging in as %s...' % jinni['username']
-    jinni.post('http://www.jinni.com/jinniLogin', {'user': jinni['username'], 'pass': jinni['password'], 'loginOverlayURL': '', 'loginSource': 'loginOverlay'})
+    j.post('http://www.jinni.com/jinniLogin', {'user': jinni['username'], 'pass': jinni['password'], 'loginOverlayURL': '', 'loginSource': 'loginOverlay'})
     print 'Logged in as %s' % jinni['username']
-    return jinni
+    return j
 
 
 def getLinkRatings(page=1, viewstate=None, total_pages=None):
-    links = {}
-
-    r = jinni.post('http://www.jinni.com/user/%s/ratings/' % jinni['username'], data={'userRatingForm': 'userRatingForm', 'userRatingForm:j_id269': 'idx%s' % page, 'javax.faces.ViewState': viewstate})
+    titles = []
+    r = j.post('http://www.jinni.com/user/%s/ratings/' % jinni['username'], data={'userRatingForm': 'userRatingForm', 'userRatingForm:j_id269': 'idx%s' % page, 'javax.faces.ViewState': viewstate})
 
     ratings_page = BeautifulSoup(r.text)
 
@@ -33,29 +58,47 @@ def getLinkRatings(page=1, viewstate=None, total_pages=None):
         if div.find(id=re.compile('likelyOrNotForMeContainer\[\d+\]')).get('style') == 'display: none':
             link = 'http://www.jinni.com%s' % div.a.get('href')
             rating = div.find(class_='digitRate').string[:-10]
-            links[link] = rating
+            # TODO: timestamp
+            titles.append(Title(link, rating))
 
     if page < total_pages:
         viewstate = ratings_page.find(id='javax.faces.ViewState').get('value')
-        links += getLinkRatings(page + 1, viewstate, total_pages)
+        titles += getLinkRatings(page + 1, viewstate, total_pages)
 
-    return links
+    return titles
 
 
-def getIMDBIDof(link):
-    movie_page = BeautifulSoup(jinni.get(link).text)
-    imdbidindex = int(str(movie_page).find('http://www.imdb.com/title/')) + 26
-    imdbid = str(movie_page)[imdbidindex:imdbidindex+9]
+def submitSeenMovies(titles):
+    jsondata = {'username': trakt['username'],
+                'password': hashlib.sha1(trakt['password']).hexdigest(),
+                'movies': [{'imdb_id': title.imdbid} for title in titles if title.titletype == 'movie' or not title.titletype]}
+    r = requests.post('http://api.trakt.tv/movie/seen/%s' % (trakt['api_key']), json.dumps(jsondata))
+    print r.text
 
-    return imdbid
+    skipped = [entry['imdb_id'] for entry in r.json()['skipped_movies']]
+    [title.setType('show') if title.imdbid in skipped else title.setType('movie') for title in titles]
+
+
+def submitSeenShows(titles):
+    for title in titles:
+        if title.titletype == 'show':
+            jsondata = {'username': trakt['username'],
+                        'password': hashlib.sha1(trakt['password']).hexdigest(),
+                        'imdb_id': title.imdbid}
+            r = requests.post('http://api.trakt.tv/show/seen/%s' % (trakt['api_key']), json.dumps(jsondata))
+            print r.text
 
 
 def main():
-    global jinni
-    jinni = openJinniSession()
-    ratings = getLinkRatings()
-    for link in ratings.keys():
-        ratings[getIMDBIDof(link)] = ratings.pop(link)
+    global j
+    j = openJinniSession()
+    titles = getLinkRatings()
+
+    for title in titles:
+        title.getIMDbID()
+
+    submitSeenMovies(titles)
+    submitSeenShows(titles)
 
 if __name__ == '__main__':
     main()
